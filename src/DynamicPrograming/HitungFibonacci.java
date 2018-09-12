4e49c47405ea37543f6f13c887fd14aa928bd977
SKIPPER <SKIPPER@LAPTOP-N1HCSIJC>
2018-09-12 13:06:05 +0700

    Dynamic Programming - Fibonacci

--- src/DynamicPrograming/HitungFibonacci.java
+++ src/DynamicPrograming/HitungFibonacci.java
@@ -15,6 +15,7 @@ public class HitungFibonacci {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil (n, hasil);
     }
     private static int tampilInput()
     {
@@ -37,6 +38,10 @@ public class HitungFibonacci {
             hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
         return hasil[n-1];
+    }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-"+n+" : "+ hasil);
     }
     
     
