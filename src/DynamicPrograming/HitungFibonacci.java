a7ef357a00ab98a47fb96a595eac8abeb5408e4e
SKIPPER <SKIPPER@LAPTOP-N1HCSIJC>
2018-09-12 12:50:42 +0700

    Dynamic Programming - Fibonacci

---
+++ src/DynamicPrograming/HitungFibonacci.java
@@ -0,0 +1,17 @@
+package DynamicPrograming;
+
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas :"+ identitas);
+        
+        System.out.println("\nHitung fibonacci");
+        System.out.println("1,1,2,3,5,8,13,21, ... dst.\n");
+    }
+    public static void main(String[] args)
+    {
+        String identitas = "Adhy Pramudhia Prabowo / XRPL3 / 01";
+        tampilJudul(identitas);
+    }
+    
+}
