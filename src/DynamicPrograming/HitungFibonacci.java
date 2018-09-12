a90e2f0721272198ce34f81641ba3c43140fe18e
SKIPPER <SKIPPER@LAPTOP-N1HCSIJC>
2018-09-12 13:02:40 +0700

    Dynamic Programming - Fibonacci

--- src/DynamicPrograming/HitungFibonacci.java
+++ src/DynamicPrograming/HitungFibonacci.java
@@ -1,5 +1,6 @@
 package DynamicPrograming;
-
+import java.math.BigInteger;
+import java.util.*;
 public class HitungFibonacci {
     private static void tampilJudul(String identitas)
     {
@@ -12,6 +13,31 @@ public class HitungFibonacci {
     {
         String identitas = "Adhy Pramudhia Prabowo / XRPL3 / 01";
         tampilJudul(identitas);
+        int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
+    private static int tampilInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = sc.nextInt();
+        
+        return n;
+    }
+    private static BigInteger fibo(int n)
+    {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        return hasil[n-1];
+    }
+    
     
 }
